e102103a227215b763f861776c6745e24fc0ab04
Alphatyrant <martinsraulino@gmail.com>
2022-11-20 23:00:40 -0300

    A first implementation to use the echoserver

---
+++ echoserver/echoserver/Client.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Hello, this is the client side!\n");
+
+// Create socket comunication
+Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+// Atempt to conect into the socket
+try
+{
+    socket.Connect("127.0.0.1", 11001);
+    byte[] buffer_envio = Encoding.Default.GetBytes("Winter is Coming");
+    socket.Send(buffer_envio);
+    byte[] buffer_receive = new byte[1024];
+    int receive_size = socket.Receive(buffer_envio);
+    Console.WriteLine(Encoding.Default.GetString(buffer_receive));
+}
+catch (Exception)
+{
+    Console.WriteLine("Erro ao conectar no servidor\n");
+    Console.WriteLine("Cheque se o server está online ou a porta esta aberta\n");
+    return;
+    //throw;
+
+}
+
+
---
+++ echoserver/server/Server.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+
+int size;
+byte[] buffer;
+Socket accept_socket, listen_socket;
+
+buffer = new byte[4096];
+
+Console.WriteLine("Hello, this is the server side!\n");
+IPEndPoint endPoint_Listen = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11001);
+listen_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+
+listen_socket.Bind(endPoint_Listen);
+listen_socket.Listen(1000);
+
+accept_socket = listen_socket.Accept();
+size = accept_socket.Receive(buffer);
+
+accept_socket.Send(buffer, size, SocketFlags.None);
+
+accept_socket.Close();
+listen_socket.Close();
